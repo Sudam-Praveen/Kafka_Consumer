71a83b951560c64f736304a46ef77077cbf7aee0
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-07-07 21:42:32 +0530

    adding kafka consumer configurations

--- Event_Consumer/src/main/java/edu/icet/crm/config/KafkaConsumerConfig.java
+++ Event_Consumer/src/main/java/edu/icet/crm/config/KafkaConsumerConfig.java
@@ -1,4 +1,35 @@
 package edu.icet.crm.config;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaConsumerConfig {
+    @Value(value = "${spring.kafka.bootstrap-server}")
+    private String kafkaServerUrl;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configs);
+    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String ,String> kafkaListener(){
+        ConcurrentKafkaListenerContainerFactory<String ,String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
