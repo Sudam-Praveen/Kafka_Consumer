642e87cbf2db502228528de35e6d01c340d897f2
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-07-07 22:18:25 +0530

    change event topic name similar to the producer

--- Event_Consumer/src/main/java/edu/icet/crm/consumer/KafkaConcumer.java
+++ Event_Consumer/src/main/java/edu/icet/crm/consumer/KafkaConcumer.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class KafkaConcumer {
-    @KafkaListener(topics = "sample-topic",groupId = "my-group")// like api methods listen to this topic and group id
+    @KafkaListener(topics = "event-msg",groupId = "my-group")// like api methods listen to this topic and group id
     public void listen(String message){
        log.info("Consumer Listener : {} ",message);
     }
